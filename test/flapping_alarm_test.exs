b4e07de96ee91ca6955ee56a06b6ee3f750f7815
Digit <aaron.shea@smartrent.com>
2023-11-08 12:43:58 -0800

    Add clear and negative test for flapping alarms

--- test/flapping_alarm_test.exs
+++ test/flapping_alarm_test.exs
@@ -14,7 +14,7 @@ defmodule FlappingAlarmTest do
       :ok = Monitor.register_new_alarm(alarm_config)
     end
 
-    test "should raise where rule conditions are met" do
+    test "should raise where rule conditions are met, and clear properly" do
       alarm_type = :flapping
       alarm_name = :test_flapping_alarm
       options = [interval: 5_000, threshold: 3]
@@ -30,6 +30,24 @@ defmodule FlappingAlarmTest do
       :alarm_handler.set_alarm(alarm_name)
 
       assert_receive %PropertyTable.Event{property: [^alarm_name, :raised]} = _event, @timeout
+
+      :alarm_handler.clear_alarm(alarm_name)
+
+      assert_receive %PropertyTable.Event{property: [^alarm_name, :cleared]} = _event, @timeout
     end
+  end
+
+  test "should not raise if conditions are not met" do
+    alarm_type = :flapping
+    alarm_name = :test_flapping_alarm
+    options = [interval: @timeout, threshold: 3]
+    alarm_config = {alarm_type, alarm_name, options}
+    :ok = Monitor.register_new_alarm(alarm_config)
+
+    Alarmist.subscribe(alarm_name)
+
+    :alarm_handler.set_alarm(alarm_name)
+
+    refute_receive _, @timeout + 150
   end
 end
