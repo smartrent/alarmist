bbc8d6c70a3e7d96b4f27fbaa319040ddd3849cc
Digit <aaron.shea@smartrent.com>
2023-11-13 15:58:49 -0800

    Revise conditions for flapping alarms

--- lib/rules/flapping.ex
+++ lib/rules/flapping.ex
@@ -4,13 +4,14 @@ defmodule Alarmist.Rules.Flapping do
 
   Config example:
   ```
-  # Configuration for a flapping alarm that will raise if set more than 5 times in a single 10 second interval
   [
+    # Configuration for a flapping alarm that will raise, if set -> cleared more than 5 times in a 10 second interval
     {:flapping, :flapping_alarm, [interval: 10_000, threshold: 5]}
   ]
   ```
 
-  Flapping alarms are raised when the alarm is set using `:alarm_handler.set_alarm(alarm_name)` more than `:threshold` times in an `:interval` period.
+  Flapping alarms will raise when they have be set and cleared successively more than `:threshold` times in an `:interval` period.
+  They will clear automatically when the threshold is not met in any single interval.
   """
   @behaviour Alarmist.Rules.Rule
 
@@ -48,28 +49,44 @@ defmodule Alarmist.Rules.Flapping do
   end
 
   @impl Rule
-  def on_set({:flapping, name, options}, _monitor_state) do
-    defaults = default_options()
-    threshold = Keyword.get(options, :threshold, defaults[:threshold])
+  def on_set({:flapping, name, _options}, _monitor_state) do
+    last_event = PropertyTable.get(Alarmist.Storage, [name, :last_event], :none)
+    :ok = PropertyTable.put(Alarmist.Storage, [name, :last_event], :set)
 
-    current_counter_value = PropertyTable.get(Alarmist.Storage, [name, :counter], 0)
-
-    if current_counter_value >= threshold do
-      # Counter is at the configured threshold, raise the alarm
-      [{:raise, name}, {:increment_counter, name}]
-    else
+    if last_event == :none or last_event == :clear do
+      # We count the first "set" as an event
+      # We count a "set" after a "clear" as an event
       [{:increment_counter, name}]
+    else
+      # Duplicate sets without a "clear" between are not counted as an event
+      []
     end
   end
 
   @impl Rule
-  @spec on_clear({:flapping, any(), any()}, any()) :: [{:clear, any()}, ...]
   def on_clear({:flapping, name, _options}, _monitor_state) do
-    [{:clear, name}]
+    PropertyTable.put(Alarmist.Storage, [name, :last_event], :clear)
+    []
   end
 
   @impl Rule
-  def on_check({:flapping, name, _options}, _monitor_state) do
-    [{:reset_counter, name}]
+  def on_check({:flapping, name, options}, _monitor_state) do
+    defaults = default_options()
+    threshold = Keyword.get(options, :threshold, defaults[:threshold])
+    current_alarm_state = PropertyTable.get(Alarmist.Storage, [name, :status], :clear)
+    current_event_count = PropertyTable.get(Alarmist.Storage, [name, :counter], 0)
+
+    cond do
+      # Within this interval, if we fell below the threshold, and the alarm is raised, clear it
+      current_alarm_state == :raised and current_event_count < threshold ->
+        [{:clear, name}, {:reset_counter, name}]
+
+      # Within this interval, if we went above the threshold and we're not raised, raise
+      current_alarm_state == :clear and current_event_count >= threshold ->
+        [{:raise, name}, {:reset_counter, name}]
+
+      true ->
+        [{:reset_counter, name}]
+    end
   end
 end
--- test/flapping_alarm_test.exs
+++ test/flapping_alarm_test.exs
@@ -3,43 +3,47 @@ defmodule FlappingAlarmTest do
 
   alias Alarmist.Monitor
 
-  @timeout 500
+  @timeout 1_000
 
   describe "Flapping alarms" do
     test "should register" do
       alarm_type = :flapping
-      alarm_name = :test_flapping_alarm
-      options = [interval: 5_000, threshold: 3]
+      alarm_name = :test_flapping_alarm1
+      options = [interval: @timeout, threshold: 3]
       alarm_config = {alarm_type, alarm_name, options}
       :ok = Monitor.register_new_alarm(alarm_config)
     end
 
     test "should raise where rule conditions are met, and clear properly" do
       alarm_type = :flapping
-      alarm_name = :test_flapping_alarm
-      options = [interval: 5_000, threshold: 3]
+      alarm_name = :test_flapping_alarm2
+      options = [interval: @timeout, threshold: 3]
       alarm_config = {alarm_type, alarm_name, options}
       :ok = Monitor.register_new_alarm(alarm_config)
 
       Alarmist.subscribe(alarm_name)
 
-      # Set more than 3 times in the specified interval
-      :alarm_handler.set_alarm(alarm_name)
-      :alarm_handler.set_alarm(alarm_name)
-      :alarm_handler.set_alarm(alarm_name)
-      :alarm_handler.set_alarm(alarm_name)
+      # Set and cleared more than 3 times in 1 second
+      Enum.each(1..3, fn _ ->
+        :alarm_handler.set_alarm(alarm_name)
+        :alarm_handler.clear_alarm(alarm_name)
+        Process.sleep(50)
+      end)
 
-      assert_receive %PropertyTable.Event{property: [^alarm_name, :raised]} = _event, @timeout
+      assert_receive %PropertyTable.Event{property: [^alarm_name, :raised]} = _event,
+                     @timeout + 500
 
-      :alarm_handler.clear_alarm(alarm_name)
+      # Alarm should clear itself within the next interval
+      Process.sleep(@timeout)
 
-      assert_receive %PropertyTable.Event{property: [^alarm_name, :cleared]} = _event, @timeout
+      assert_receive %PropertyTable.Event{property: [^alarm_name, :cleared]} = _event,
+                     @timeout + 500
     end
   end
 
   test "should not raise if conditions are not met" do
     alarm_type = :flapping
-    alarm_name = :test_flapping_alarm
+    alarm_name = :test_flapping_alarm3
     options = [interval: @timeout, threshold: 3]
     alarm_config = {alarm_type, alarm_name, options}
     :ok = Monitor.register_new_alarm(alarm_config)
