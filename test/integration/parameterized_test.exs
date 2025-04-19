26424294c7250e9749eeb7959310a6791f6016df
Frank Hunleth <fhunleth@troodon-software.com>
2025-04-19 17:26:16 -0600

    WIP: support multiple Alarmist's

--- test/integration/parameterized_test.exs
+++ test/integration/parameterized_test.exs
@@ -3,38 +3,40 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 defmodule Integration.ParameterizedTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
-  setup do
-    AlarmUtilities.cleanup()
+  setup context do
+    name = context.test
+    start_link_supervised!({Alarmist.Supervisor, name: name})
 
-    on_exit(fn -> AlarmUtilities.assert_clean_state() end)
+    [alarmist: name]
   end
 
-  test "parameterized identity" do
-    Alarmist.subscribe({IdentityTuple1Alarm, :_})
-    Alarmist.add_managed_alarm({IdentityTuple1Alarm, "eth0"})
-    Alarmist.add_managed_alarm({IdentityTuple1Alarm, "wlan0"})
+  test "parameterized identity", %{alarmist: name} do
+    Alarmist.subscribe(name, {IdentityTuple1Alarm, :_})
+    Alarmist.add_managed_alarm(name, {IdentityTuple1Alarm, "eth0"})
+    Alarmist.add_managed_alarm(name, {IdentityTuple1Alarm, "wlan0"})
     refute_received _
 
-    :alarm_handler.set_alarm({{IdentityTupleTriggerAlarm, "eth0"}, nil})
+    Alarmist.set_alarm(name, {{IdentityTupleTriggerAlarm, "eth0"}, nil})
     assert_receive %Alarmist.Event{id: {IdentityTuple1Alarm, "eth0"}, state: :set}
 
-    :alarm_handler.set_alarm({{IdentityTupleTriggerAlarm, "wlan0"}, nil})
+    Alarmist.set_alarm(name, {{IdentityTupleTriggerAlarm, "wlan0"}, nil})
     assert_receive %Alarmist.Event{id: {IdentityTuple1Alarm, "wlan0"}, state: :set}
 
-    :alarm_handler.clear_alarm({IdentityTupleTriggerAlarm, "eth0"})
+    Alarmist.clear_alarm(name, {IdentityTupleTriggerAlarm, "eth0"})
     assert_receive %Alarmist.Event{id: {IdentityTuple1Alarm, "eth0"}, state: :clear}
 
-    :alarm_handler.clear_alarm({IdentityTupleTriggerAlarm, "wlan0"})
+    Alarmist.clear_alarm(name, {IdentityTupleTriggerAlarm, "wlan0"})
     assert_receive %Alarmist.Event{id: {IdentityTuple1Alarm, "wlan0"}, state: :clear}
 
     refute_receive _
-    Alarmist.remove_managed_alarm({IdentityTuple1Alarm, "eth0"})
-    Alarmist.remove_managed_alarm({IdentityTuple1Alarm, "wlan0"})
+    Alarmist.remove_managed_alarm(name, {IdentityTuple1Alarm, "eth0"})
+    Alarmist.remove_managed_alarm(name, {IdentityTuple1Alarm, "wlan0"})
+    AlarmUtilities.assert_clean_state(name)
   end
 
-  test "parameterized trigger alarm" do
+  test "parameterized trigger alarm", %{alarmist: name} do
     defmodule WiredEthernetAlarm do
       use Alarmist.Alarm
 
@@ -43,78 +45,82 @@ defmodule Integration.ParameterizedTest do
       end
     end
 
-    Alarmist.add_managed_alarm(WiredEthernetAlarm)
-    Alarmist.subscribe(WiredEthernetAlarm)
+    Alarmist.add_managed_alarm(name, WiredEthernetAlarm)
+    Alarmist.subscribe(name, WiredEthernetAlarm)
     refute_received _
 
-    :alarm_handler.set_alarm({{NetworkTriggerAlarm, "eth0"}, nil})
+    Alarmist.set_alarm(name, {{NetworkTriggerAlarm, "eth0"}, nil})
     assert_receive %Alarmist.Event{id: WiredEthernetAlarm, state: :set}
 
-    :alarm_handler.set_alarm({{NetworkTriggerAlarm, "wlan0"}, nil})
+    Alarmist.set_alarm(name, {{NetworkTriggerAlarm, "wlan0"}, nil})
     refute_received _
 
-    :alarm_handler.clear_alarm({NetworkTriggerAlarm, "eth0"})
+    Alarmist.clear_alarm(name, {NetworkTriggerAlarm, "eth0"})
     assert_receive %Alarmist.Event{id: WiredEthernetAlarm, state: :clear}
 
-    :alarm_handler.clear_alarm({NetworkTriggerAlarm, "wlan0"})
+    Alarmist.clear_alarm(name, {NetworkTriggerAlarm, "wlan0"})
     refute_receive _
 
-    Alarmist.remove_managed_alarm(WiredEthernetAlarm)
+    Alarmist.remove_managed_alarm(name, WiredEthernetAlarm)
+    AlarmUtilities.assert_clean_state(name)
   end
 
-  test "parameterized subscription with temporaries" do
-    Alarmist.subscribe({CompoundTuple1Alarm, :_})
-    Alarmist.add_managed_alarm({CompoundTuple1Alarm, "eth0"})
-    Alarmist.add_managed_alarm({CompoundTuple1Alarm, "wlan0"})
+  test "parameterized subscription with temporaries", %{alarmist: name} do
+    Alarmist.subscribe(name, {CompoundTuple1Alarm, :_})
+    Alarmist.add_managed_alarm(name, {CompoundTuple1Alarm, "eth0"})
+    Alarmist.add_managed_alarm(name, {CompoundTuple1Alarm, "wlan0"})
     assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "eth0"}, state: :set}
     assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "wlan0"}, state: :set}
 
-    :alarm_handler.set_alarm({{CompoundTuple1Trigger2Alarm, "eth0"}, nil})
+    Alarmist.set_alarm(name, {{CompoundTuple1Trigger2Alarm, "eth0"}, nil})
     assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "eth0"}, state: :clear}
 
-    :alarm_handler.set_alarm({{CompoundTuple1TriggerAlarm, "eth0"}, nil})
+    Alarmist.set_alarm(name, {{CompoundTuple1TriggerAlarm, "eth0"}, nil})
     refute_receive _
 
-    :alarm_handler.set_alarm({GlobalTriggerAlarm, nil})
+    Alarmist.set_alarm(name, {GlobalTriggerAlarm, nil})
     assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "eth0"}, state: :set}
 
-    :alarm_handler.clear_alarm({CompoundTuple1TriggerAlarm, "eth0"})
+    Alarmist.clear_alarm(name, {CompoundTuple1TriggerAlarm, "eth0"})
 
-    Alarmist.remove_managed_alarm({CompoundTuple1Alarm, "eth0"})
-    Alarmist.remove_managed_alarm({CompoundTuple1Alarm, "wlan0"})
+    Alarmist.remove_managed_alarm(name, {CompoundTuple1Alarm, "eth0"})
+    assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "eth0"}, state: :clear}
+    Alarmist.remove_managed_alarm(name, {CompoundTuple1Alarm, "wlan0"})
     assert_receive %Alarmist.Event{id: {CompoundTuple1Alarm, "wlan0"}, state: :clear}
-    :alarm_handler.clear_alarm(GlobalTriggerAlarm)
-    :alarm_handler.clear_alarm({CompoundTuple1Trigger2Alarm, "eth0"})
+    Alarmist.clear_alarm(name, GlobalTriggerAlarm)
+    Alarmist.clear_alarm(name, {CompoundTuple1Trigger2Alarm, "eth0"})
+    AlarmUtilities.assert_clean_state(name)
   end
 
-  test "two parameter identity" do
-    Alarmist.subscribe({IdentityTuple2Alarm, :_, :_})
-    Alarmist.add_managed_alarm({IdentityTuple2Alarm, "param1", "param2"})
+  test "two parameter identity", %{alarmist: name} do
+    Alarmist.subscribe(name, {IdentityTuple2Alarm, :_, :_})
+    Alarmist.add_managed_alarm(name, {IdentityTuple2Alarm, "param1", "param2"})
     refute_received _
 
-    :alarm_handler.set_alarm({{IdentityTupleTriggerAlarm, "param1", "param2"}, nil})
+    Alarmist.set_alarm(name, {{IdentityTupleTriggerAlarm, "param1", "param2"}, nil})
     assert_receive %Alarmist.Event{id: {IdentityTuple2Alarm, "param1", "param2"}, state: :set}
 
-    :alarm_handler.clear_alarm({IdentityTupleTriggerAlarm, "param1", "param2"})
+    Alarmist.clear_alarm(name, {IdentityTupleTriggerAlarm, "param1", "param2"})
     assert_receive %Alarmist.Event{id: {IdentityTuple2Alarm, "param1", "param2"}, state: :clear}
 
     refute_receive _
-    Alarmist.remove_managed_alarm({IdentityTuple2Alarm, "param1", "param2"})
+    Alarmist.remove_managed_alarm(name, {IdentityTuple2Alarm, "param1", "param2"})
+    AlarmUtilities.assert_clean_state(name)
   end
 
-  test "three parameter identity" do
-    Alarmist.subscribe({IdentityTuple3Alarm, :_, :_, :_})
-    Alarmist.add_managed_alarm({IdentityTuple3Alarm, "param1", "param2", "param3"})
+  test "three parameter identity", %{alarmist: name} do
+    Alarmist.subscribe(name, {IdentityTuple3Alarm, :_, :_, :_})
+    Alarmist.add_managed_alarm(name, {IdentityTuple3Alarm, "param1", "param2", "param3"})
     refute_received _
 
-    :alarm_handler.set_alarm({{IdentityTupleTriggerAlarm, "param1", "param2", "param3"}, nil})
+    Alarmist.set_alarm(name, {{IdentityTupleTriggerAlarm, "param1", "param2", "param3"}, nil})
 
     assert_receive %Alarmist.Event{
       id: {IdentityTuple3Alarm, "param1", "param2", "param3"},
       state: :set
     }
 
-    :alarm_handler.clear_alarm({IdentityTupleTriggerAlarm, "param1", "param2", "param3"})
+    Alarmist.clear_alarm(name, {IdentityTupleTriggerAlarm, "param1", "param2", "param3"})
 
     assert_receive %Alarmist.Event{
       id: {IdentityTuple3Alarm, "param1", "param2", "param3"},
@@ -122,6 +128,7 @@ defmodule Integration.ParameterizedTest do
     }
 
     refute_receive _
-    Alarmist.remove_managed_alarm({IdentityTuple3Alarm, "param1", "param2", "param3"})
+    Alarmist.remove_managed_alarm(name, {IdentityTuple3Alarm, "param1", "param2", "param3"})
+    AlarmUtilities.assert_clean_state(name)
   end
 end
